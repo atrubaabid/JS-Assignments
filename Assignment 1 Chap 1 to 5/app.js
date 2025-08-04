f7d32d5f1118016a06ea9cb4be44d1bfbdb7b056
atrubaabid <atrubaabid7@gmail.com>
2025-08-03 20:45:22 -0700

    Assignment 1

---
+++ Assignment 1 Chap 1 to 5/app.js
@@ -0,0 +1,46 @@
+// CHAPTER 1
+// Q1______________________
+// alert("Welcome");
+
+// Q2______________________
+// alert("Error! please enter a valid password");
+
+// Q3______________________
+// alert("Welcome to JS \nHappy Coding!");
+
+// Q4______________________
+// alert("Welcome to JS land...");
+
+// ON CONSOLE
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs "); 
+
+// Q5______________________
+// alert("Hello...l can run JS through my web browser's console"); 
+
+// Q6______________________
+    // in HTML file
+
+
+// Q7______________________
+// in HTML file
+
+
+// ------------------CHAP-1-COMPLETE--------------------------------
+
+// CHAPTER 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
