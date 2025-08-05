8c7f86aa3f8fe3889ecc5c12f91e0a1744d98fe9
atrubaabid <atrubaabid7@gmail.com>
2025-08-05 12:01:47 -0700

    update 3

--- Assignment 1 Chap 1 to 5/app.js
+++ Assignment 1 Chap 1 to 5/app.js
@@ -100,13 +100,8 @@
 //     fatherName,
 //     name2;
 
-
-
 // illegal
 // var @name , #name, 123Nmae, * name2, @1name,
-
-
-
 
 // Q3
 // document.write("Rules for naming JS variables <br><br><br><br>");
@@ -115,22 +110,69 @@
 // document.write("Variable names are case Sensitive <br>");
 // document.write("Variable names should not be JS Keywords <br>");
 
-
-
 // ------------------CHAP-4-COMPLETE--------------------------------
-
 
 // CHAPTER 5
 
 // Q1
 // var a = 3, b=5;
 // var Addition = a+b;
-// document.write("sum of " + a + " and " + b + " is " + Addition);
+// document.write("sum of " + a + " and " + b + " is " + Addition + "<br>");
 
 // Q2
 
+// var Subtraction = a-b;
+// document.write("Subtraction of " + a + " and " + b + " is " + Subtraction + "<br>");
+
+// var Multiplication = a*b;
+// document.write("Multiplication of " + a + " and " + b + " is " + Multiplication + "<br>");
+
+// var Division = a/b;
+// document.write("Division of " + a + " and " + b + " is " + Division + "<br>");
+
+// var Modulus = a%b;
+// document.write("Modulus of " + a + " and " + b + " is " + Modulus + "<br>");
+
+// Q3
+// var a;
+// document.write("Value after variable declaration is: " + a + "<br>");
+// a = 5;
+// document.write("Initial value:" + a + "<br>");
+// a++;
+// document.write("Value after increment is: " + a + "<br>");
+// a+=7;
+// document.write("Value after addition is: " + a + "<br>");
+// a--;
+// document.write("Value after decrement is: " + a + "<br>");
+// a%=3;
+// document.write("The remainder is: " + a + "<br>");
+
+// Q4
+// var ticket = 600 *5;
+// document.write("Total cost to buy 5 tickets to a movie is " + ticket + "PKR");
+
+// Q5
+// var num = 4;
+// document.write("Table of 4 <br>");
+// document.write("4 x 1 = " + num*1 + "<br>");
+// document.write("4 x 2 = " + num*2 + "<br>");
+// document.write("4 x 3 = " + num*3 + "<br>");
+// document.write("4 x 4 = " + num*4 + "<br>");
+// document.write("4 x 5 = " + num*5 + "<br>");
+// document.write("4 x 6 = " + num*6 + "<br>");
+// document.write("4 x 7 = " + num*7 + "<br>");
+// document.write("4 x 8 = " + num*8 + "<br>");
+// document.write("4 x 9 = " + num*9 + "<br>");
+// document.write("4 x 10 = " + num*10 + "<br>");
+
+// Q6
+// var Celsius = 25;
+// var Fahrenheit = 70;
+// var CelsiusFor = (Fahrenheit - 32) * 5 / 9;
+// var FahrenheitFor = (Celsius * 9) / 5 + 32;
+// document.write(Celsius + "<sup>o</sup>C is " + FahrenheitFor + "<sup>o</sup>F <br>");
+// document.write(Fahrenheit + "<sup>o</sup>F is " + CelsiusFor + "<sup>o</sup>C ");
 
 
-
-
+// Q7
 
