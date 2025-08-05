3b98e6744c02c136d51c714490ce1c03324369df
atrubaabid <atrubaabid7@gmail.com>
2025-08-04 23:47:38 -0700

    task added

--- Assignment 1 Chap 1 to 5/app.js
+++ Assignment 1 Chap 1 to 5/app.js
@@ -12,32 +12,122 @@
 // alert("Welcome to JS land...");
 
 // ON CONSOLE
-// alert("Happy Coding! \nPrevent this page from creating additional dialogs "); 
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs ");
 
 // Q5______________________
-// alert("Hello...l can run JS through my web browser's console"); 
+// alert("Hello...l can run JS through my web browser's console");
 
 // Q6______________________
-    // in HTML file
-
+// in HTML file
 
 // Q7______________________
 // in HTML file
 
-
 // ------------------CHAP-1-COMPLETE--------------------------------
 
 // CHAPTER 2
+// Q1
+// var username;
+
+// Q2
+// var myName = "Syeda Atruba";
+
+// Q3
+// var message = "Hello world"
+// alert(message);
+
+// Q4
+// var name = "Atruba";
+// var age = 19;
+// var course = "web development"
+// alert(name);
+// alert(age);
+// alert(course);
+
+// Q5
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza);
+
+// Q6
+// var email = "syedaatruba@gmail.com"
+// alert("My email is " + email);
+
+// Q7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book " + book);
+
+// Q8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Q9
+//  var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+//  alert(design);
+
+// ------------------CHAP-2-COMPLETE--------------------------------
+
+// CHAPTER 3
+
+// Q1
+// var age = 20;
+// alert("I'm "+ age+ " years old");
+
+// Q2
+// var n = 14;
+// alert("you have visited this site " + n + " times")
+
+// Q3
+// var birthYear = 2004;
+// document.write("my birth year is " + birthYear + "<br> Datatype of my declared  variable is number")
+
+// Q4
+// var VisitorName = "Zoha";
+// var ProductTitle = "T-shirts";
+// var Quantity = 5;
+// document.write( VisitorName + " ordered " + Quantity + " " + ProductTitle + " on XYZ Clothing store. ");
+
+// ------------------CHAP-3-COMPLETE--------------------------------
+
+// CHAPTER 4
+
+// Q1
+// var a = 1, b = 2, c = 3;
+
+// Q2
+// legal
+// var myName,
+//     $age,
+//     _course,
+//     fatherName,
+//     name2;
+
+
+
+// illegal
+// var @name , #name, 123Nmae, * name2, @1name,
 
 
 
 
+// Q3
+// document.write("Rules for naming JS variables <br><br><br><br>");
+// document.write("Variable names can only contain numbers, $ and _. For example $my_1stVariable <br>");
+// document.write("Variables must begin with a letter, $ or _. For example $name, _name or name <br>");
+// document.write("Variable names are case Sensitive <br>");
+// document.write("Variable names should not be JS Keywords <br>");
 
 
 
+// ------------------CHAP-4-COMPLETE--------------------------------
 
 
+// CHAPTER 5
 
+// Q1
+// var a = 3, b=5;
+// var Addition = a+b;
+// document.write("sum of " + a + " and " + b + " is " + Addition);
+
+// Q2
 
 
 
