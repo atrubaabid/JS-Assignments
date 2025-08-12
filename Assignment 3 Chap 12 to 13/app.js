d5e36fe4e28e2cacf27e1e17c44fdb4b2ab01da1
atrubaabid <atrubaabid7@gmail.com>
2025-08-12 15:53:41 -0700

    completed

---
+++ Assignment 3 Chap 12 to 13/app.js
@@ -0,0 +1,86 @@
+// Q1
+
+// var char = prompt("Enter any character");
+// if (char.length !== 1) {
+//   document.write("please enter only one character");
+// } else {
+//   var res = char.charCodeAt(0);
+
+//   if (res >= 65 && res <= 90) {
+//     document.write(char + " is upercase");
+//   } else if (res >= 97 && res <= 122) {
+//     document.write(char + " is lowercase");
+//   }
+// }
+
+// Q2
+
+// var num1 = +prompt("Enter number 1 : ");
+// var num2 = +prompt("Enter number 2 : ");
+// if (num1 > num2) {
+//   document.write(num1 + "num1 is greater");
+// } else if (num1 < num2) {
+//   document.write(num2 + "num2 is greater");
+// } else if (num1 === num2) {
+//   document.write(num1 + " and  " + num2 + " both are equal");
+// }
+
+// Q3
+
+// var number = +prompt("Enter number to check its  positive, negative or zero.");
+// if (number > 0) {
+//   document.write("The number is positive.");
+// } else if (number < 0) {
+//   document.write("The number is negative.");
+// } else if (number === 0) {
+//   document.write("The number is zero.");
+// } else {
+//   document.write("Invalid input. Please enter a valid number.");
+// }
+
+// Q4
+
+// var char = prompt("Enter any Character");
+// if (char.length !== 1) {
+//   document.write("Please enter only one character");
+// } else {
+//   if (
+//     char === "a" ||
+//     char === "e" ||
+//     char === "i" ||
+//     char === "o" ||
+//     char === "u"
+//   ) {
+//     document.write("True");
+//   } else {
+//     document.write("False");
+//   }
+// }
+
+// Q5
+
+// var password = "atruba786";
+// var userPass = prompt("Enter your password");
+// if (userPass.length === 0) {
+//   document.write("Please enter your password");
+// } else {
+//   if (password === userPass) {
+//     document.write(
+//       "Correct! The password you entered matches the original password"
+//     );
+//   } else if (password !== userPass) {
+//     document.write("Incorrect password");
+//   }
+// }
+
+// Q6
+
+// var greeting;
+// var hour = 19;
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// Q7
