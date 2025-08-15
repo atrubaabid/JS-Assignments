813ac2ab119ec5f2d81eecbd6e9c42f6e4625868
atrubaabid <atrubaabid7@gmail.com>
2025-08-15 15:38:57 -0700

    pending

---
+++ Assignment 5 Chap 14 to 16/app.js
@@ -0,0 +1,49 @@
+// Q1
+//  var studentName = [];
+
+// Q2
+// let studentNames = new Array();
+
+// Q3
+// var stringsArray = ["apple", "banana" , "Mango", "Orange"];
+
+// Q4
+// var numberArry= [11,22,33,44,55,66];
+
+// Q5
+// var booleanArray = [true, false, true, false];
+
+// Q6
+// var mixedarry = ["apple", 12, true, null];
+
+// Q7
+// var EduQualification = [
+//   "SSC",
+//   "HSC",
+//   "BCS",
+//   "BS",
+//   "BCOM",
+//   "MS",
+//   "M . Phil",
+//   "PhD",
+// ];
+
+// document.write("Qualification <br>");
+// for (var i = 0; i < EduQualification.length; i++) {
+//   document.write(i + ") " + EduQualification[i] + "<br>");
+// }
+
+// Q8
+// var studentName = ["Atruba", "Rida", "Raza"];
+// var studentScore = [480, 330, 230];
+// var totalMarks = 500;
+
+
+// for (let i = 0; i < studentName.length; i++) {
+//   var Percentage = (studentScore[i] / totalMarks) * 100;
+//   document.write("Score of " + studentName[i] +" is "+ studentScore[i] +". Percentage "+ Percentage + "% <br>");
+// }
+
+// Q9
+
+
