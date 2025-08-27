1d006277b6df452d8ad589c2b5443fcbaea61e1e
atrubaabid <atrubaabid7@gmail.com>
2025-08-26 18:47:15 -0700

    comp

---
+++ Assignment 6 Chap 17 to 20/app.js
@@ -0,0 +1,21 @@
+
+// Q7
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var user = prompt("welcome to our bakery what do you want to order");
+// var flag = false;
+// for (i = 0; i < A.length ; i++) {
+
+//     if(A[i]===user){
+//         document.write( A[i] +  " is availbale at index "+ i);
+//         flag===true;
+//         break;
+//     }
+
+// }
+
+// if(flag){
+//     document.write("we are sorry "+user+ " is not vailable in our bakery");
+// }
+
+
