904b9ab317f8320574d8b51521d0edf75b390c6a
atrubaabid <atrubaabid7@gmail.com>
2025-09-12 14:15:00 -0700

    Completed

--- Assignment 6 Chap 17 to 20/app.js
+++ Assignment 6 Chap 17 to 20/app.js
@@ -1,3 +1,75 @@
+// Q1
+// var a = [[]];
+
+// Q2
+// var a = [[0,1,2,3], [1,0,1,2],[2,1,0,1]];
+
+// for(i=0; i<a.length; i++){
+
+//     for(j=0; j<a[i].length; j++){
+
+//         document.write(a[j].join("") + "<br>");
+//     }
+
+// }
+
+// Q3
+
+// for(var i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// Q4
+
+// var userinput = +prompt("Enter a number for table print");
+// var length = +prompt("Enter length of the table");
+
+// console.log("Multiplication Table of " + userinput + " length " + length );
+
+// for(i=1; i<=length; i++){
+//     console.log(userinput + " x " + i + " = " + userinput*i);
+
+// }
+
+// Q5
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for(i=0; i<fruits.length; i++){
+//     console.log(fruits[i]);
+//     console.log("Element at index " + i + " is " + fruits[i]);
+
+// }
+
+// Q6
+
+// var counting = [];
+// console.log("COUNTING");
+// for(i=1; i<=15; i++){
+//     console.log(counting.push(i));
+// }
+
+// var reverse = [];
+// console.log("REVERSE");
+// for(i=10; i>=1; i--){
+//     console.log(i);
+// }
+
+// console.log("EVEN");
+// for(i=0; i<=20; i += 2){
+//     console.log(i);
+
+// }
+
+// console.log("ODD");
+// for(i=1; i<=20; i += 2){
+//     console.log(i);
+
+// }
+
+// console.log("SERIES");
+// for(i=2; i<=20; i += 2){
+//     console.log(i + "k");
+// }
 
 // Q7
 
@@ -12,10 +84,59 @@
 //         break;
 //     }
 
+//     if(flag){
+//         document.write("we are sorry "+user+ " is not vailable in our bakery");
+//     }
 // }
 
-// if(flag){
-//     document.write("we are sorry "+user+ " is not vailable in our bakery");
+
+// Q8
+
+// var A = [24, 53, 78, 91, 12];
+// console.log(A);
+
+// var largest = A[0];
+// for(i=1; i<=A.length; i++){
+//     if(A[i]>largest){
+//         largest = A[i];
+//     }
 // }
+
+// console.log(largest);
+
+// Q9
+
+// var A = [24, 53, 78, 91, 12];
+// console.log(A);
+
+// var largest = A[0];
+// for(i=1; i<=A.length; i++){
+//     if(A[i]<largest){
+//         largest = A[i];
+//     }
+// }
+
+// console.log(largest);
+
+
+// Q10
+
+// for(i=1; i<=100; i++){
+//     if(i%5===0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
