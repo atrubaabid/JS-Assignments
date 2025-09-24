f26dce7b73acc323c54cb67e64efa0c1c84133ce
atrubaabid <atrubaabid7@gmail.com>
2025-09-23 18:07:03 -0700

    pending

---
+++ Assignment 7 Chap 21 to 25/app.js
@@ -0,0 +1,10 @@
+// Q1
+
+// var firstName = prompt("ENTER YOR FIRST NAME");
+// var lastName = prompt("ENTER YOR LAST NAME");
+// var fullName = firstName + " " + lastName;
+// alert("Good Morning " + fullName);
+
+
+
+
---
+++ Assignment 9 Chap 31 to 34/app.js
@@ -0,0 +1,94 @@
+// Q1
+//  var date = new Date();
+//  console.log(date);
+
+//  Q2
+// var months = [
+//   "JANUARY",
+//   "FEBRURY",
+//   "MARCH",
+//   "APRIL",
+//   "MAY",
+//   "JUN",
+//   "JULY",
+//   "AUGUST",
+//   "SEPTEMBER",
+//   "OCTOBER",
+//   "NOVEMBER",
+//   "DECEMBER",
+// ];
+// var date = new Date();
+// var month = date.getMonth();
+// var currentMonth = months[month];
+// alert(currentMonth);
+
+
+// Q3
+// var days=["SUN","MON", "TUE","WED", "THU", "FRI", "SAT"];
+// var date = new Date();
+// var day = date.getDay();
+// var currentDay = days[day];
+// alert("Today is " + currentDay);
+
+
+
+// Q4
+
+// var days=["SUN","MON", "TUE","WED", "THU", "FRI", "SAT"];
+// var date = new Date();
+// var day = date.getDay();
+// if(days === 0 && days === 6){
+//     alert("its fun day")
+// }else{
+//     alert("its study day")
+// }
+
+// console.log(days[0] +" "+ days[6]);
+
+
+// Q5
+
+// var fulldate = new Date();
+// var date = fulldate.getDate();
+// if(date < 16){
+//     console.log("First fifteen days of the month");
+    
+// }else if(date >=16 ){
+//     console.log("Last days of the month");
+
+// }
+
+
+
+
+// Q6
+
+
+
+
+
+
+
+
+
+
+
+
+// Q7
+
+var fulldate = new Date();
+var time = fulldate.getTime();
+console.log(time);
+
+
+
+
+
+
+
+
+
+
+
+
+
