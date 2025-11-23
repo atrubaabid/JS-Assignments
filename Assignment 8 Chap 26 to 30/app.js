42462a6cb9ccfb02dd2b2611566f71365bf71c3d
atrubaabid <atrubaabid7@gmail.com>
2025-11-23 14:57:32 +0500

    Completed

---
+++ Assignment 8 Chap 26 to 30/app.js
@@ -0,0 +1,105 @@
+// Q1
+
+
+// let userInput = +prompt("Enter any number");
+
+// console.log(userInput);
+// console.log(Math.round(userInput));
+// console.log(Math.floor(userInput));
+// console.log(Math.ceil(userInput));
+
+
+// Q2
+
+// let num = +prompt("Enter a negative floating-point number:");
+
+// console.log(num);
+// console.log(Math.round(num));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+
+// Q3
+
+// let num = +prompt("Enter a number:");
+// console.log(`The absolut value of ${num} is ${Math.abs(num)}` );
+
+
+// Q4
+
+
+// let diceValue =  Math.floor(Math.random()*6)+1;
+// let diceValue2 =  Math.floor(Math.random()*6)+1;
+// console.log(diceValue);
+// console.log(diceValue2);
+
+// Q5
+
+// let coin = Math.floor(Math.random()*2)+1;
+// if(coin == 1){
+//     console.log("Tail " + coin);
+
+// }else{
+//     console.log("Heads " + coin);
+
+// }
+
+
+
+// // Generate a random number: 0 or 1
+// let coin = Math.random() < 0.5 ? "Heads" : "Tails";
+
+// // Display the result in the browser
+// document.write("Coin toss result: " + coin);
+
+
+// Q6
+
+// let random = Math.floor(Math.random()*100)+1;
+// console.log(random);
+
+// Q7
+
+// let user = parseFloat(prompt("Enter your weight"));
+
+// if (isNaN(user)) {
+//     console.log("Enter valid no");
+
+// } else {
+//     console.log("The weight of user is " + user + " Kilograms");
+
+// }
+
+// Q8
+
+// let secretno = Math.floor(Math.random()*10)+1;
+// let user = +prompt("Guess secret no between 1 to 10");
+// if(user === secretno){
+//     console.log("CONGRATULATIONS "  + user + " "+ secretno);
+    
+// }else{
+//     console.log("Try Again "  + user + " "+ secretno);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
