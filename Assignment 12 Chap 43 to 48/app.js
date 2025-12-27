b5c4a0afe141d414f617346588a82f5de454f3df
atrubaabid <atrubaabid7@gmail.com>
2025-12-27 20:02:17 +0500

    Completed

---
+++ Assignment 12 Chap 43 to 48/app.js
@@ -0,0 +1,23 @@
+// Q3
+
+// function deleteRow(button) {
+//   var row = button.parentNode.parentNode;
+//   row.remove();
+// }
+
+
+
+var counter = 0;
+
+function increase() {
+  counter++;
+  document.getElementById("counterValue").innerHTML = counter;
+}
+
+function decrease() {
+  counter--;
+  document.getElementById("counterValue").innerHTML = counter;
+}
+
+
+
