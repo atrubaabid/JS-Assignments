a502a341037e3c111a047da1fc5f59a7131d785b
atrubaabid <atrubaabid7@gmail.com>
2026-01-06 21:56:05 +0500

    woo

---
+++ Assignment 14 Chap 58 to 67/app.js
@@ -0,0 +1,103 @@
+// Question 1
+
+// Get element of id “main-content” and assign them in a variable.
+
+// let mainContent = document.querySelector("#main-content");
+
+
+
+// Display all child elements of “main-content” element.
+
+// let children = mainContent.children;
+// console.log(children);
+
+// for (i = 0; i < children.length; i++) {
+//     document.writeln(
+//         children[i].innerText + "<br>"
+//     )
+// }
+
+// Get all elements of class “render” and show their innerHTML 
+// in browser.
+
+// let render = document.querySelectorAll(".render");
+// for (i = 0; i < render.length; i++) {
+//     document.writeln(render[i].innerHTML + "<br>");
+// }
+
+
+// Fill input value whose element id first-name using javascript.
+
+// document.querySelector("#first-name").value = "ALEX";
+
+// Repeat part iv for id ”last-name” and “email”.
+
+// document.querySelector("#last-name").value = "ALEXAAAAA";
+// document.querySelector("#email").value = "alexa@gmail.com";
+
+
+
+// Question 2
+
+
+
+// What is node type of  element having id “form-content”.
+// let div = document.querySelector("#form-content");
+// console.log(div.nodeType);
+// console.log(div.firstChild.nodeType);
+
+
+
+// Show node type of element having id “lastName” and its child node.
+
+// let LN = document.querySelector("#last-name");
+// console.log(LN.nodeType);
+// console.log(LN.firstChild.nodeType);
+
+// iii. Update child node of element having id “lastName”.
+// let res = LN.firstChild.nodeValue = "Atruba";
+// console.log(res);
+
+
+
+// iv. Get First and last child of id “main-content”.
+
+// let mainContent = document.querySelector("#main-content");
+// let fc = mainContent.firstElementChild;
+// let lc = mainContent.lastElementChild;
+// console.log(fc);
+// console.log(lc);
+
+
+
+
+
+// v. Get next and previous siblings of id “lastName”.
+
+// let LN = document.querySelector("#last-name");
+// console.log(LN.nextSibling);
+// console.log(LN.previousSibling);
+// console.log(LN.nextElementSibling);
+// console.log(LN.previousElementSibling);
+
+
+
+
+
+
+
+
+
+// vi. Get parent node and node type of element having id “email”
+
+// let email = document.querySelector("#email");
+// console.log(email.parentNode.nodeType);
+
+// let pn = email.parentNode.nodeName;
+// console.log(pn);
+
+
+
+
+
+
